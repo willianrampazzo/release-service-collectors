0c3cc9dc08d6510835fe1e61c8e904c6451d8759
galitf <galfrid@gmail.com>
2025-01-17 10:52:06 +0200

    feat(CPAAS-5843): search for JIRA issues (#1)
    Co-authored-by: Galit Rosenthal <grosenth@redhat.com>

---
+++ lib/get_issues.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+python lib/get_issue.py \
+    tenant \
+     --url https://issues.redhat.com \
+    --query 'project = KONFLUX AND status = "NEW"' \
+     --credentials-file ../cred-file.json
+"""
+
+import argparse
+import json
+import os
+import requests
+
+
+def search_issues():
+    parser = argparse.ArgumentParser(description='Get all issues from Jira query')
+    parser.add_argument("mode", choices=["managed", "tenant"], help="Mode in which the script is called. It does not have any impact for this script.")
+    parser.add_argument('-u','--url', help='URL to Jira', required=True)
+    parser.add_argument('-q','--query', help='Jira qrl query', required=True)
+    parser.add_argument('-c','--credentials-file', help='Path to credentials file', required=True)
+    args = vars(parser.parse_args())
+
+    if ( not os.path.isfile(args['credentials_file'])):
+       print(f"ERROR: Path to credentials file {args['credentials_file']} doesn't exists")
+       exit(1)
+
+    return query_jira(args['url'], args['query'], args['credentials_file']) 
+
+
+def parse_credentials_file(credentials_file):
+    """
+    format credentials file:
+    {
+         "api_token": "token_id"
+    }
+    """
+    # Open and read the JSON file
+    with open(credentials_file, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def query_jira(jira_domain_url, jql_query, credentials_file):
+    credentials = parse_credentials_file(credentials_file)
+    api_token = credentials["api_token"]
+
+    # Define the endpoint URL
+    url = f'{jira_domain_url}/rest/api/2/search'
+
+    # Define your JQL query and other parameters
+    # example of jql query:
+    # 'project = "KONFLUX" AND status = "To Do"'
+    start_at = 0
+    max_results = 50
+    fields = ['summary', 'status', 'assignee']
+
+    # Construct the JSON payload
+    data = {
+        'jql': jql_query,
+        'startAt': start_at,
+        'maxResults': max_results,
+        'fields': fields
+    }
+
+    # Create the headers
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + api_token
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        data=json.dumps(data),
+    )
+
+    # Check the response
+    list_issues = []
+    if response.status_code == 200:
+        issues = response.json()['issues']
+        for issue in issues:
+            list_issues.append(issue["key"])
+    else:
+        print(f"ERROR: Failed to retrieve data. HTTP Status Code: {response.status_code}")
+        exit(1)
+
+    return list_issues
+
+
+if __name__ == "__main__":
+    return_list_issues = search_issues()
+    print(return_list_issues)
---
+++ tests/test_get_issues.py
@@ -0,0 +1,94 @@
+import os
+import pytest
+import requests
+from collections import namedtuple
+from pytest import MonkeyPatch
+
+import lib.get_issues
+from lib.get_issues import query_jira, parse_credentials_file
+
+
+MockResponse = namedtuple('MockResponse',['status_code','json'])
+
+def mock_isfile(file):
+    return true
+
+
+def mock_parse_credentials_file(file):
+    data = {
+        "api_token": "mock_api_token_key",
+    }
+    return data
+
+
+def test_parse_credentials_file(monkeypatch):
+    monkeypatch.setitem(globals(), 'parse_credentials_file', mock_parse_credentials_file)
+    results = parse_credentials_file('file')
+    assert results == {
+        "api_token": "mock_api_token_key",
+    }
+    assert results["api_token"] == "mock_api_token_key"
+
+
+def mock_empty_query_jira(url, query, file):
+    return []
+
+
+# empty response data
+mock_reponse_data_empty = { 'issues': [] }
+
+def test_query_jira_empty_response(monkeypatch):
+    monkeypatch.setattr(os.path, 'isfile', mock_isfile)
+    monkeypatch.setattr(lib.get_issues, 'parse_credentials_file', mock_parse_credentials_file)
+    monkeypatch.setattr(lib.get_issues, 'query_jira', mock_empty_query_jira)
+
+    def mock_post(url, headers, data):
+        # Simulating a successful response with no issues (HTTP 200)
+        return MockResponse(status_code=200, json=lambda: mock_reponse_data_empty)
+
+    monkeypatch.setattr(requests, 'post', mock_post)
+    result = query_jira("https://mock-domain.com", "project = TEST", "mock_credentials_file.json")
+    assert result == []
+
+
+def mock_fail_query_jira(url, query, file):
+    exit(1)
+
+
+mock_reponse_data_failure = { 'issues': [] }
+
+# Test case for failed API response
+def test_query_jira_failure(monkeypatch):
+    monkeypatch.setattr(os.path, 'isfile', mock_isfile)
+    monkeypatch.setattr(lib.get_issues, 'parse_credentials_file', mock_parse_credentials_file)
+    monkeypatch.setattr(lib.get_issues, 'query_jira', mock_fail_query_jira)
+
+    def mock_post(url, headers, data):
+        # Simulate a failed response (HTTP 500)
+        return MockResponse(status_code=500, json=lambda: mock_reponse_data_failure)
+
+    monkeypatch.setattr(requests, 'post', mock_post)
+    with pytest.raises(SystemExit):
+        query_jira("https://mock-domain.com", "project = TEST", "mock_credentials_file.json")
+
+
+mock_reponse_data_success = {
+                'issues': [
+                    {"key": "KONFLUX-1"},
+                    {"key": "KONFLUX-2"}
+                ]
+            }
+
+# Test case for successful API response
+def test_query_jira_success(monkeypatch):
+    monkeypatch.setattr(os.path, 'isfile', mock_isfile)
+    monkeypatch.setattr(lib.get_issues, 'parse_credentials_file', mock_parse_credentials_file)
+    def mock_post(url, headers, data):
+        # Simulate a successful response (HTTP 200)
+        return MockResponse(status_code=200, json=lambda: mock_reponse_data_success)
+
+    monkeypatch.setattr(requests, 'post', mock_post)
+
+    result = query_jira("https://mock-domain.com", "project = TEST", "mock_credentials_file.json")
+
+    assert result == ["KONFLUX-1", "KONFLUX-2"]
