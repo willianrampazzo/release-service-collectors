d27f116d9dad68f73e65b8506c2134586b08d4cd
Willian Rampazzo <willianr@redhat.com>
2025-08-26 15:49:04 -0300

    Add Hello World collector
    Signed-off-by: Willian Rampazzo <willianr@redhat.com>

---
+++ lib/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+python lib/hello.py \
+    tenant \
+    --release release.json \
+    --previousRelease previous_release.json
+"""
+
+if __name__ == "__main__":
+    print("{'message': 'Hello world!'}")
