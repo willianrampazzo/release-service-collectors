6e9e2f5f3f8a7e352bc559018478980da3d09cc5
Willian Rampazzo <willianr@redhat.com>
2025-10-07 15:05:16 -0300

    Change hello world to sleep
    Signed-off-by: Willian Rampazzo <willianr@redhat.com>

--- lib/hello.py
+++ lib/hello.py
@@ -5,6 +5,22 @@ python lib/hello.py \
     --release release.json \
     --previousRelease previous_release.json
 """
+import argparse
+import time
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "mode",
+        choices=["managed", "tenant"],
+        help="Mode in which the script is called. It does not have any impact for this script."
+    )
+    parser.add_argument('-s', '--sec', help='Seconds to sleep', required=True)
+    parser.add_argument('-r', '--release', help='Path to current release file. Not used, supported to align the interface.', required=True)
+    parser.add_argument('-p', '--previousRelease', help='Path to previous release file. Not used, supported to align the interface.', required=False)
+    args = vars(parser.parse_args())
+
+    sec = int(args["sec"])
+    print(f"Sleeping for {sec}s")
+    time.sleep(sec)
     print("{'message': 'Hello world!'}")
